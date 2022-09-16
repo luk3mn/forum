356bccc444a9ed2013b59a3450341decbdf27937
luk3mn <lucasrenan2015@gmail.com>
2022-09-16 20:45:21 -0300

    Construção da lógica de login e iniciando a lógica de cadastro de usuario

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='autenticacao.login'),
     path('cadastro/', views.cadastro, name='autenticacao.cadastro'),
+    path('logout/', views.logout, name='autenticacao.logout'),
 ]
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 def login(request):
 
@@ -8,8 +10,50 @@ def login(request):
         usuario = request.POST.get('usuario') 
         senha = request.POST.get('senha')
 
-        return HttpResponse(usuario)
+        # prepara o usuario e senha para o login
+        usuario_logado = auth.authenticate(username=usuario, password=senha)
+
+        if usuario_logado: # verifica se usuario e senha existem
+            auth.login(request, usuario_logado) # realiza o login
+            return redirect('/')
+        else: # caso não encontre o usuário e a senha informados
+            return HttpResponse("Usuário e/ou senha incorretos")
+    elif request.method == "GET": # impedi que o usuário logado redirecione para a pagina de login/cadastro
+        if request.user.is_authenticated:
+            return redirect('/')
+    
     return render(request, 'autenticacao/signin.html')
 
 def cadastro(request):
+
+    if request.method == "POST":
+
+        # coleta dos dados
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        usuario = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        confirme_senha = request.POST.get('confirme-senha')
+
+        if not senha == confirme_senha:
+            # return redirect('/autenticacao/cadastro')
+            return HttpResponse('Senhas não coincidem!')
+        
+        if not (nome and sobrenome and usuario and senha and confirme_senha):
+            return redirect('/autenticacao/cadastro')
+        
+        if User.objects.filter(username=usuario):
+            return HttpResponse("Usuario já existe")
+            
+
+        return HttpResponse([nome, sobrenome, usuario, senha])
+    elif request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
+
+
     return render(request, 'autenticacao/signup.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
